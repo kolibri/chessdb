056b3d7e96c6c92bf56ded46eddea2a7aeb227e7
ko <entengelb@gmail.com>
2016-09-16 21:48:41 +0200

    homepage and redirect to locale fallback

---
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function homepageAction()
+    {
+        return $this->render(
+            'default/homepage.html.twig'
+        );
+    }
+
+    public function indexAction(Request $request)
+    {
+        return $this->redirectToRoute('app_default_homepage');
+    }
+}
